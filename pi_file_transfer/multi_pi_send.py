e5eb0c6868308e5fac3bfa56ee9f3048105b0a4b
sarin-jacob <sarin.jacob@niser.ac.in>
2021-02-25 20:59:40 +0530

    multi client server with log

---
+++ pi_file_transfer/multi_pi_send.py
@@ -0,0 +1,51 @@
+def filez(conn,addr):
+    import socket
+    import tqdm
+    import os
+    from time import ctime ,sleep
+    try:
+        print(f'{addr} is connected !')
+        with open('receiver_log.txt','a') as log:
+                log.write(f'\nTime :{ctime()} -> {addr} is connected !')
+        data=conn.recv(4096).decode()
+        filename,filesize=data.split('+')
+        filename=os.path.basename(filename)
+        filesize=int(filesize)
+        progress=tqdm.tqdm(range(filesize),f'Receiving {filename}',unit='B',unit_scale=True,unit_divisor=1024)
+        with open(filename,'wb') as f:
+            while True:
+                read=conn.recv(4096)
+                if not read:
+                    conn.close()
+                    break
+                f.write(read)
+                progress.update(len(read))
+            # sleep(10)
+            print(f'\n {filename} done receiving !')
+            with open('receiver_log.txt','a') as log:
+                if filesize-int(os.path.getsize(filename))<=(filesize/100):
+                    log.write(f"\nTime :{ctime()} -> {filename} ({filesize}B) received from {addr}")
+                else:
+                    log.write(f"\nTime :{ctime()} -> {filename} ({filesize}B) received from {addr} is 'broken'")
+    except Exception as e:
+        with open('receiver_log.txt','a') as log:
+            log.write(f'Time :{ctime()} -> {e}\n')
+        conn.close()
+def reciever(port=1310):   
+    import socket
+    import tqdm
+    import os
+    from _thread import start_new_thread
+    host=socket.gethostbyname(socket.gethostname())
+    host1='0.0.0.0'
+    print('\n',host,'is the servers pi address',end='\n\n')
+    with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
+        s.bind((host1,port))
+        s.listen(5)
+        print(f" Listening as {host} : {port}")
+        while True:       
+            conn,addr =s.accept()
+            start_new_thread(filez,(conn,addr)) 
+        
+        
+reciever()
