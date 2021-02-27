a99aaec055fa4b28452870aa5bae0ecce12070e0
sarin-jacob <sarin.jacob@niser.ac.in>
2021-02-28 00:21:49 +0530

    client part makes sure file is send

--- pi_file_transfer/client.py
+++ pi_file_transfer/client.py
@@ -2,15 +2,18 @@ def sender(filename,ip,port=1310):
     import socket
     import os
     filesize=os.path.getsize(filename)
+    k=0
     with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
-        s.connect((ip,port))
-        s.send(f"{filename}+{filesize}".encode())
-        with open(filename,'rb') as f:
-            while True:
-                read=f.read(4096)
-                if not read:
-                    print(f"{os.path.basename(filename)} done transfering !")
-                    s.close()
-                    break
-                s.sendall(read)
+        while k=0
+            s.connect((ip,port))
+            s.send(f"{filename}+{filesize}".encode())
+            with open(filename,'rb') as f:
+                while True:
+                    read=f.read(4096)
+                    if not read:
+                        print(f"{os.path.basename(filename)} done transfering !")
+                        k=1
+                        s.close()
+                        break
+                    s.sendall(read)
 sender('file','ip')
