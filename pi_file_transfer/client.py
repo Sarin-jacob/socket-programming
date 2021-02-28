e262af9a1a2714e5aa65b213da7561b444ae56fe
sarin-jacob <sarin.jacob@niser.ac.in>
2021-02-28 21:57:21 +0530

    chumma

--- pi_file_transfer/client.py
+++ pi_file_transfer/client.py
@@ -5,15 +5,18 @@ def sender(filename,ip,port=1310):
     k=0
     with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
         while k==0:
-            s.connect((ip,port))
-            s.send(f"{filename}+{filesize}".encode())
-            with open(filename,'rb') as f:
-                while True:
-                    read=f.read(4096)
-                    if not read:
-                        print(f"{os.path.basename(filename)} done transfering !")
-                        k=1
-                        s.close()
-                        break
-                    s.sendall(read)
+            try:
+                s.connect((ip,port))
+                s.send(f"{filename}+{filesize}".encode())
+                with open(filename,'rb') as f:
+                    while True:
+                        read=f.read(4096)
+                        if not read:
+                            print(f"{os.path.basename(filename)} done transfering !")
+                            k=1
+                            s.close()
+                            break
+                        s.sendall(read)
+            except:
+                pass
 sender('file','ip')
